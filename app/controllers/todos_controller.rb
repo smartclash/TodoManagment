e06154c990efb701623a5bd3403361143169f881
Karan Sanjeev <yo@alphaman.me>
2021-01-20 11:57:23 +0530

    Create todo logic completed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,18 @@ class TodosController < ApplicationController
     id = params[:id]
     render plain: Todo.find(id).to_pleasent_string
   end
+
+  def create
+    due = Date.parse(params[:due])
+    text = params[:text]
+    completed = params[:completed]
+
+    new_todo = Todo.create!(
+      todo_text: text,
+      due_date: due,
+      completed: completed
+    )
+
+    render plain: "Created a new todo with ID #{new_todo.id}"
+  end
 end
