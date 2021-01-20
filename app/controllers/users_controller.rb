db57cae2a10f8e924a92a9e2f259c0ea771ccef5
Karan Sanjeev <yo@alphaman.me>
2021-01-20 12:39:11 +0530

    Boilerplate code for users feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController
+  def login
+    #
+  end
+
+  def index
+    #
+  end
+
+  def create
+    #
+  end
+
+  def show
+    #
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :todos
+  resources :users
+
+  post 'users/login', to: 'users#login'
 end
