40469e3f228d480f8b61a1185a1bc29fb3b5968b
Karan Sanjeev <yo@alphaman.me>
2021-01-20 23:48:56 +0530

    Completed migration and integrated the new design

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,20 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? '[X]' : '[ ]'
     "#{is_completed} #{id}. #{due_date.to_s(:long)} #{todo_text}"
   end
+
+  def self.overdue
+    all.where('due_date < ?', Date.today)
+  end
+
+  def self.due_today
+    all.where('due_date = ?', Date.today)
+  end
+
+  def self.due_later
+    all.where('due_date > ?', Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
 end
