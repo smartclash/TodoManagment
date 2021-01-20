61e2c1e3167ea8f982987bea81e0066c8a115fbe
Karan Sanjeev <yo@alphaman.me>
2021-01-20 12:01:00 +0530

    Convert create todo to post method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
-  get 'todos/create', to: 'todos#create'
   get 'todos/:id', to: 'todos#show'
+
+  post 'todos/create', to: 'todos#create'
 end
