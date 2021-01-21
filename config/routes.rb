ca0f5eab0bb4a78e4e255f8e3863f2745d560a56
Karan Sanjeev <yo@alphaman.me>
2021-01-21 16:52:11 +0530

    Add default index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/', to: 'home#index'
+  post 'users/login', to: 'users#login'
+
   resources :todos
   resources :users
-
-  post 'users/login', to: 'users#login'
 end
