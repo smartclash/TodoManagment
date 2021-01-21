4cbd5d8e95dfa01f2069101ea103554ee462f198
Karan Sanjeev <yo@alphaman.me>
2021-01-21 18:50:53 +0530

    Rename password column

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_01_21_104700) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
