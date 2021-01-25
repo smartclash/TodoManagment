1db62206e6ab6a9e0cf503bdaeeaade7e63351a5
Karan Sanjeev <yo@alphaman.me>
2021-01-25 20:06:23 +0530

    Prevent empty records or duplicate email during signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,16 @@ class UsersController < ApplicationController
     last_name = params[:last_name]
     first_name = params[:first_name]
 
-    User.create!(email: email, first_name: first_name, last_name: last_name, password: password)
-    redirect_to home_path
+    user = User.new(email: email, first_name: first_name, last_name: last_name, password: password)
+
+    if user.save
+      redirect_to home_path
+    else
+      flash[:title] = 'Just a small problem'
+      flash[:error] = user.errors.full_messages.join(', ')
+
+      redirect_back fallback_location: new_user_path
+    end
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :todos
+
+  validates :first_name, presence: true
+  validates :email, { presence: true, uniqueness: true }
 end
