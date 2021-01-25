3442b8432a4ad3e99508ec5c2f964481c6c127aa
Karan Sanjeev <yo@alphaman.me>
2021-01-25 19:11:35 +0530

    Make conditionals clean

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :ensure_user_logged_in
 
   def ensure_user_logged_in
-    unless current_user
-      redirect_to home_path
-    end
+    redirect_to home_path unless current_user
   end
 
   def current_user
     return @current_user if @current_user
 
     user_id = session[:user_id]
-    if user_id
-      @current_user = User.find(user_id)
-    end
+    @current_user = User.find(user_id) if user_id
   end
 end
